8627fee186ff4f90e99c4036e726e99282de9688
kgbuquerin <sec.gomez@protonmail.com>
2022-03-24 15:50:21 +0100

    feat: read file and print techniques

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/attack-defend-go
 
 go 1.17
+
+require golang.org/x/exp v0.0.0-20220323204016-c86f0da35e87 // indirect
--- main.go
+++ main.go
@@ -5,7 +5,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-	"net/http"
+	//"net/http"
+	"golang.org/x/exp/slices"
 	"time"
 )
 
@@ -69,56 +70,122 @@ type EnterpriseAttack struct {
 	SpecVersion string `json:"spec_version"`
 }
 
+type AdversaryJson struct {
+	Name     string `json:"name"`
+	Versions struct {
+		Attack    string `json:"attack"`
+		Navigator string `json:"navigator"`
+		Layer     string `json:"layer"`
+	} `json:"versions"`
+	Domain      string `json:"domain"`
+	Description string `json:"description"`
+	Filters     struct {
+		Platforms []string `json:"platforms"`
+	} `json:"filters"`
+	Sorting int `json:"sorting"`
+	Layout  struct {
+		Layout              string `json:"layout"`
+		AggregateFunction   string `json:"aggregateFunction"`
+		ShowID              bool   `json:"showID"`
+		ShowName            bool   `json:"showName"`
+		ShowAggregateScores bool   `json:"showAggregateScores"`
+		CountUnscored       bool   `json:"countUnscored"`
+	} `json:"layout"`
+	HideDisabled bool `json:"hideDisabled"`
+	Techniques   []struct {
+		TechniqueID       string        `json:"techniqueID"`
+		Tactic            string        `json:"tactic"`
+		Score             int           `json:"score"`
+		Color             string        `json:"color"`
+		Comment           string        `json:"comment"`
+		Enabled           bool          `json:"enabled"`
+		Metadata          []interface{} `json:"metadata"`
+		Links             []interface{} `json:"links"`
+		ShowSubtechniques bool          `json:"showSubtechniques"`
+	} `json:"techniques"`
+	Gradient struct {
+		Colors   []string `json:"colors"`
+		MinValue int      `json:"minValue"`
+		MaxValue int      `json:"maxValue"`
+	} `json:"gradient"`
+	LegendItems                   []interface{} `json:"legendItems"`
+	Metadata                      []interface{} `json:"metadata"`
+	Links                         []interface{} `json:"links"`
+	ShowTacticRowBackground       bool          `json:"showTacticRowBackground"`
+	TacticRowBackground           string        `json:"tacticRowBackground"`
+	SelectTechniquesAcrossTactics bool          `json:"selectTechniquesAcrossTactics"`
+	SelectSubtechniquesWithParent bool          `json:"selectSubtechniquesWithParent"`
+}
+
 func main() {
-    endpoint := "https://raw.githubusercontent.com/mitre/cti/master/enterprise-attack/enterprise-attack.json"
+	/*
+	   endpoint := "https://raw.githubusercontent.com/mitre/cti/master/enterprise-attack/enterprise-attack.json"
 
-    client := http.Client {
-        Timeout: time.Second * 2, // timeout after 2 seconds
-    }
+	   client := http.Client {
+	       Timeout: time.Second * 2, // timeout after 2 seconds
+	   }
 
-    req, reqErr := http.NewRequest(http.MethodGet, endpoint, nil)
-    if reqErr != nil {
-        log.Fatal(reqErr)
-    }
-    res, doErr := client.Do(req)
-    if doErr != nil {
-        log.Fatal(doErr)
-    }
-    if res.Body != nil {
-        defer res.Body.Close()
-    }
+	   req, reqErr := http.NewRequest(http.MethodGet, endpoint, nil)
+	   if reqErr != nil {
+	       log.Fatal(reqErr)
+	   }
+	   res, doErr := client.Do(req)
+	   if doErr != nil {
+	       log.Fatal(doErr)
+	   }
+	   if res.Body != nil {
+	       defer res.Body.Close()
+	   }
 
-    stix, readErr := ioutil.ReadAll(res.Body)
-    if readErr != nil {
-        log.Fatal(readErr)
-    }
+	   stix, readErr := ioutil.ReadAll(res.Body)
+	   if readErr != nil {
+	       log.Fatal(readErr)
+	   }
 
-    data := EnterpriseAttack {}
-    jsonErr := json.Unmarshal(stix, &data)
-    if jsonErr != nil {
-        log.Fatal(jsonErr)
-    }
+	   data := EnterpriseAttack {}
+	   jsonErr := json.Unmarshal(stix, &data)
+	   if jsonErr != nil {
+	       log.Fatal(jsonErr)
+	   }
+	*/
 
-    for i := 0; i < len(data.Objects); i += 1 {
-        fmt.Println(data.Objects[i].ExternalReferences)
-    }
+	content, rfErr := ioutil.ReadFile("./apt32-winnti-turla.json")
+	if rfErr != nil {
+		log.Fatal(rfErr)
+	}
 
-    // there is no reason to do it like this
-    // I need the tactics navigator export (prefered in json)
-    // here we have tactics from mitre and our threat intelligence
-    //
-    // 1. read json for threat actor or combined threat actors
-    // 2. get techniques
-    // 3. calc top techniques
-    // 4. check at d3fend for techniques (how?)
-    // 5. get defenses
-    // 6. statistics on defenses (top defense, ...)
-    // 
-    // 1: json read
-    // 2: "techniques" field
-    // 3: techniques.score
-    // 4. GET request: https://d3fend.mitre.org/api/offensive-technique/attack/<techniqueID>.json
-    // 5: TODO parsing
-    // 6: TODO statistics
+	jsonData := AdversaryJson{}
+	jsonDataErr := json.Unmarshal(content, &jsonData)
+	if jsonDataErr != nil {
+		log.Fatal(jsonDataErr)
+	}
+
+	var alreadyChecked []string
+	for i := 0; i < len(jsonData.Techniques); i += 1 {
+		if !slices.Contains(alreadyChecked, jsonData.Techniques[i].TechniqueID) {
+			fmt.Printf("ID: %s \t Score: %d\n", jsonData.Techniques[i].TechniqueID,
+				jsonData.Techniques[i].Score)
+			alreadyChecked = append(alreadyChecked, jsonData.Techniques[i].TechniqueID)
+		}
+	}
+	fmt.Printf("Found %d duplicates.\n", len(alreadyChecked))
+
+	// there is no reason to do it like this
+	// I need the tactics navigator export (prefered in json)
+	// here we have tactics from mitre and our threat intelligence
+	//
+	// 1. read json for threat actor or combined threat actors
+	// 2. get techniques
+	// 3. calc top techniques
+	// 4. check at d3fend for techniques (how?)
+	// 5. get defenses
+	// 6. statistics on defenses (top defense, ...)
+	//
+	// 1: json read
+	// 2: "techniques" field
+	// 3: techniques.score
+	// 4. GET request: https://d3fend.mitre.org/api/offensive-technique/attack/<techniqueID>.json
+	// 5: TODO parsing
+	// 6: TODO statistics
 
 }
